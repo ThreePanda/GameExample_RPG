b36c79d5a8a0a57d1107830a9a9d19c1ea7d631f
ThreePanda <79436643+ThreePanda@users.noreply.github.com>
2023-12-11 19:27:08 +0800

    Add cinemachine, post processing(Global Volume) and animator of ide->running

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -7,10 +7,11 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent _agent;
-
+    private Animator _animator;
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -18,8 +19,19 @@ public class PlayerController : MonoBehaviour
         //将移动的事件注册到单例的event事件OnMouseCliked中，随事件唤醒调用
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
     }
+
+    private void Update()
+    {
+        SwitchAnimation();
+    }
+
     public void MoveToTarget(Vector3 target)
     {
         _agent.destination = target;
+    }
+
+    public void SwitchAnimation()
+    {
+        _animator.SetFloat("Speed", _agent.velocity.sqrMagnitude);
     }
 }
